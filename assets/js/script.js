005798e2d4c6b8ab3e231f72906bc749774dbed5
HaileyThomas <haileyraebeauty@gmail.com>
2022-05-13 20:43:46 -0700

    finish inn wide styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var introContainer = document.getElementById("intro-container");
 var startBtn = document.getElementById("start-btn");
 var mainContainer = document.querySelector("#main-container");
 var menuContainerEl;
+var innContainerEl;
 
 // SOUNDS
 var menuMusic = document.getElementById("menu-audio");
@@ -207,7 +208,108 @@ function loadMenu() {
 
 // LOAD INN FUNCTION
 function loadInn() {
-  console.log("Inn function is working!");
+  // create div for inn container
+  innContainerEl = document.createElement("div");
+  innContainerEl.className = "content-container";
+  innContainerEl.setAttribute("id", "inn-container");
+  innContainerEl.setAttribute("data-state", "visible");
+
+  // STATS INFO
+  // create div for stats info
+  var statsDivEl = document.createElement("div");
+  statsDivEl.className = "stats-box";
+  // create div for health
+  var healthDivEl = document.createElement("div");
+  healthDivEl.className = "stats-element";
+  // create div for health icon
+  var healthIconDivEl = document.createElement("div");
+  healthIconDivEl.className = "health-icon";
+  healthIconDivEl.innerHTML = '<ion-icon name="heart-outline"></ion-icon>';
+  healthDivEl.appendChild(healthIconDivEl);
+  // create div for health stats
+  var healthStatsDivEl = document.createElement("div");
+  healthStatsDivEl.className = "stats-text";
+  healthStatsDivEl.textContent = playerInfo.health;
+  healthDivEl.appendChild(healthStatsDivEl);
+  // append health div to stats container
+  statsDivEl.appendChild(healthDivEl);
+  // create div for attack
+  var attackDivEl = document.createElement("div");
+  attackDivEl.className = "stats-element";
+  // create div for attack icon
+  var attackIconDivEl = document.createElement("div");
+  attackIconDivEl.className = "attack-icon";
+  attackIconDivEl.innerHTML = '<ion-icon name="flame-outline"></ion-icon>';
+  attackDivEl.appendChild(attackIconDivEl);
+  // create div for attack stats
+  var attackStatsDivEl = document.createElement("div");
+  attackStatsDivEl.className = "stats-text";
+  attackStatsDivEl.textContent = playerInfo.attack;
+  attackDivEl.appendChild(attackStatsDivEl);
+  // append attack div to stats container
+  statsDivEl.appendChild(attackDivEl);
+  // create div for defense
+  var defenseDivEl = document.createElement("div");
+  defenseDivEl.className = "stats-element";
+  // create div for defense icon
+  var defenseIconDivEl = document.createElement("div");
+  defenseIconDivEl.className = "defense-icon";
+  defenseIconDivEl.innerHTML = '<ion-icon name="shield-outline"></ion-icon>';
+  defenseDivEl.appendChild(defenseIconDivEl);
+  // create div for defense stats
+  var defenseStatsDivEl = document.createElement("div");
+  defenseStatsDivEl.className = "stats-text";
+  defenseStatsDivEl.textContent = playerInfo.defense;
+  defenseDivEl.appendChild(defenseStatsDivEl);
+  // append defense div to stats container
+  statsDivEl.appendChild(defenseDivEl);
+  // create div for money
+  var moneyDivEl = document.createElement("div");
+  moneyDivEl.className = "stats-element";
+  // create div for money icon
+  var moneyIconDivEl = document.createElement("div");
+  moneyIconDivEl.className = "money-icon";
+  moneyIconDivEl.innerHTML = '<ion-icon name="server-outline"></ion-icon>';
+  moneyDivEl.appendChild(moneyIconDivEl);
+  // create div for money stats
+  var moneyStatsDivEl = document.createElement("div");
+  moneyStatsDivEl.className = "stats-text";
+  moneyStatsDivEl.textContent = playerInfo.money;
+  moneyDivEl.appendChild(moneyStatsDivEl);
+  // append money div to stats container
+  statsDivEl.appendChild(moneyDivEl);
+  // append stats div to inn container
+  innContainerEl.appendChild(statsDivEl);
+
+  // CENTER TEXT
+  // create div for inn text
+  var innTextDivEl = document.createElement("div");
+  innTextDivEl.className = "text-box";
+  innTextDivEl.textContent =
+    "Welcome to the Inn! Would you like to spend 10 coins to gain 25 health?";
+  innContainerEl.appendChild(innTextDivEl);
+
+  // BUTTONS
+  // create div for inn buttons
+  var innBtnDivEl = document.createElement("div");
+  innBtnDivEl.className = "option-btn-container";
+  // create button for buying health
+  var innBtnYes = document.createElement("button");
+  innBtnYes.className = "option-btn-yes";
+  innBtnYes.setAttribute = ("id", "inn-btn-yes");
+  innBtnYes.textContent = "Buy Health";
+  innBtnDivEl.appendChild(innBtnYes);
+  // create button to go back to town
+  var innBtnNo = document.createElement("button");
+  innBtnNo.className = "option-btn-no";
+  innBtnNo.setAttribute = ("id", "inn-btn-no");
+  innBtnNo.textContent = "Go Back To Town";
+  innBtnDivEl.appendChild(innBtnNo);
+  // append button container to inn container
+  innContainerEl.appendChild(innBtnDivEl);
+
+  // append ALL to main container
+  mainContainer.appendChild(innContainerEl);
 }
 
 // LOAD MARKET FUNCTION
